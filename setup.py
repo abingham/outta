52f726cdc6efb16179c88591699a80674df070ad
Austin Bingham <austin.bingham@gmail.com>
2021-04-05 11:57:18 +0200

    Black formatting

--- setup.py
+++ setup.py
@@ -3,44 +3,43 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='outta',
+    name="outta",
     version="0.0.0",
-    packages=find_packages('source'),
-
-    author='Austin Bingham',
-    author_email='austin.bingham@protonmail.com',
-    description='ANSI control code parsing',
-    license='MIT',
-    keywords='',
-    url='http://github.com/abingham/outta',
+    packages=find_packages("source"),
+    author="Austin Bingham",
+    author_email="austin.bingham@protonmail.com",
+    description="ANSI control code parsing",
+    license="MIT",
+    keywords="",
+    url="http://github.com/abingham/outta",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
     ],
-    platforms='any',
+    platforms="any",
     include_package_data=True,
-    package_dir={'': 'source'},
+    package_dir={"": "source"},
     # package_data={'outta': . . .},
     install_requires=[
-        'click',
-        'lark',
-        'pyte',
+        "click",
+        "lark",
+        "pyte",
     ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax, for
     # example: $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['black', 'bump2version'],
+        "dev": ["black", "bump2version"],
         # 'doc': ['sphinx', 'cartouche'],
-        'test': ['pytest'],
+        "test": ["pytest"],
     },
     entry_points={
         # 'console_scripts': [
         #    'outta = outta.cli:main',
         # ],
     },
-    long_description=Path('README.rst').read_text(encoding='utf-8'),
+    long_description=Path("README.rst").read_text(encoding="utf-8"),
 )
