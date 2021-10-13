c1cba648e116ad816165a9295f9f0aab7916dd94
Austin Bingham <austin.bingham@gmail.com>
2021-10-13 09:19:02 +0200

    Added example mentioned in README

---
+++ docs/example.py
@@ -0,0 +1,17 @@
+from outta.parser import Parser
+
+# Here's the text to be parsed
+text = "\x1b[4COut of\x1b[3Dta control!"
+
+# Construct a Parser and feed the text in.
+parser = Parser()
+elements = tuple(parser.feed(text))
+
+# Print each of the elements
+for element in parser.feed(text):
+    print(">", element)
+
+# Reconstruct the input text from the elements and print it
+full_text = "".join(e.text for e in elements)
+assert full_text == text
+print(full_text)
