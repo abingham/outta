15df9a2c80d7d9daf2f4cc786e8af74ddeb1c39e
Austin Bingham <austin@sixty-north.com>
2021-10-15 08:34:41 +0200

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="outta",
-    version="0.2.1",
+    version="0.3.0",
     packages=find_packages("source"),
     author="Austin Bingham",
     author_email="austin.bingham@protonmail.com",
